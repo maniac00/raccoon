8d2db2b857fc77976ecf5374b982840cf26bba80
maniac00 <meniac00@gmail.com>
2024-02-22 18:50:52 +0900

    change show code

--- showSales.sh
+++ showSales.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+sleep 30
 sudo ps -ef | grep rpi | awk '{print $2}' | xargs kill -15
-result=$(python3 /root/raccoon/getSales.py)
-sudo /root/rpi-rgb-led-matrix/utils/text-scroller -f /root/raccoon/Arial.bdf -C255,255,255 -s0 -x3 -y13 --led-cols=64 --led-rows=32 --led-gpio-mapping=adafruit-hat --led-brightness=70 "$result"
 
+input_text = $(cat sales.txt)
+sudo /root/rpi-rgb-led-matrix/utils/text-scroller -f /root/raccoon/Arial.bdf -C255,255,255 -s0 -x3 -y13 --led-cols=64 --led-rows=32 --led-gpio-mapping=adafruit-hat --led-brightness=70 "$input_text"
---
+++ writeTestSales.py
@@ -0,0 +1,18 @@
+import gspread
+import sys
+
+def main():
+    # 구글 스프레드시트 환경 설정
+    my_gs_json = '/root/raccoon/raccoon-2-f83f2a09096a.json'
+    gs_account_detail = gspread.service_account(my_gs_json) #사용자 계정 json 파일 사용 및 활성화
+    main_gs_sheet = 'https://docs.google.com/spreadsheets/d/1i_ilm7ezmR7qh_PzjZBZjw7EfUH4Bezk77uZS22k7GQ/edit#gid=0' #구글 사용시트 주소
+    useSheet = gs_account_detail.open_by_url(main_gs_sheet) #사용자 계정이 사용하고자하는 시트 url 설정
+
+    # 구글 스프레드시트에서 매출값 가져오기
+    my_sheet = useSheet.worksheet('일일 매출현황') #시트명과 동일하게 기재
+    value = my_sheet.acell('A1').value.replace(',','')+'원'
+    with open("sales_now.txt", "w") as file:
+        file.write(value)
+
+if __name__ == "__main__":
+    main()
